887deff89fbf2d8fc2f64c53190edc7b8bbdfcf5
Emerson Loustau <e@thesecretaddress.com>
2018-01-19 19:13:00 -0800

    need to handle arguments

--- test/test-unix-command.js
+++ test/test-unix-command.js
@@ -4,10 +4,28 @@ const expect = require('chai').expect;
 describe('translate', function() {
 
     it('should describe a single command without options', function(done){
-        unixCommand.translate('cat').then( summary => {
-            expect(summary).to.equal('concatenate and print files');
-        });
-        done();
+        unixCommand.translate('cat').then( response => {
+            expect(response).to.equal('concatenate and print files');
+        })
+        .then(() => done(), done);
+    });
+
+    it('should split piped commands', function(done){
+        unixCommand.translate('echo "HI" | cat').then( response => {
+
+            expect(response).to.equal('write arguments to the standard output pass the output to concatenate and print files');
+        })
+        .then(done, done);
+        
+    });
+
+    it('should split redirect commands', function(done){
+        unixCommand.translate('echo "HI" >> file.txt').then( response => {
+
+            expect(response).to.equal('write arguments to the standard output append output to a file file.txt');
+        })
+        .then(done, done);
+        
     });
 
     // it('should describe a single command with options', function(done){
